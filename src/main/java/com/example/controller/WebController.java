8f3233292d8695b9b5fa6da73b564806064f38a0
Shilpa1225 <shilpakamsani@gmail.com>
2020-11-08 23:54:27 -0800

    Signed-off-by: Shilpa1225 <shilpakamsani@gmail.com>

---
+++ src/main/java/com/example/controller/WebController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author skamsani2
+ *
+ */
+@RestController
+public class WebController {
+
+	@GetMapping("/hi")
+	public String sayhai() {
+		return "helooooo";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
